42998c0f81b973528e09e81cd86cbf4e5fc5caf9
sw <1327281098@qq.com>
2023-07-07 18:14:18 +0800

    获取表单数据并校验发送axios请求

--- js/common.js
+++ js/common.js
@@ -7,3 +7,13 @@ const Toast = (msg) => {
   toast.show()
   document.querySelector('.toast-body').innerHTML = msg
 }
+//获取表单数据并校验发送axios请求
+document.querySelector('#btn-register').addEventListener('click', async (e) => {
+  const data = serialize(document.querySelector('register-form'), { hash: true, empty: true })
+  if (!data.username) return Toast('请输入用户名')
+  if (data.username.length < 8 || data.username.length > 30) return Toast('用户名长度为8-30位')
+  if (!data.password) return Toast('请输入密码')
+  if (data.password.length < 6 || data.password.length > 30) return Toast('密码长度为6-30位')
+  const res = await axios.post('/api/reguser', data)
+  Toast(res.data.message)
+})
