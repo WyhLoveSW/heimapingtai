2782fa3d843968021478e3735aa71e2448343683
sw <1327281098@qq.com>
2023-07-10 14:43:33 +0800

    初始化Echarts渲染柱形图

--- js/index.js
+++ js/index.js
@@ -6,8 +6,9 @@ showUsername()
 logout()
 //渲染统计数据 获取数据 渲染到页面上
 const getData = async () => {
-  const { data: { overview } } = await axios.get('/dashboard')
+  const { data: { overview, year } } = await axios.get('/dashboard')
   renderOverview(overview)
+  renderYear(year)
 }
 getData()
 
@@ -16,4 +17,97 @@ const renderOverview = (overview) => {
   Object.keys(overview).forEach(item => {
     document.querySelector(`.${item}`).innerHTML = overview[item]
   })
+}
+
+// 柱状图
+const renderYear = (year) => {
+  console.log(year);
+  var myChart = echarts.init(document.querySelector('#line'))
+  option = {
+    title: {
+      text: '2022全学科走势',
+      left: 10,
+      top: 10,
+    },
+    xAxis: {
+      type: 'category',
+      data: year.map(item => item.month),
+      axisLine: {
+        lineStyle: { type: 'dashed', color: '#cccccc' }
+      }
+    },
+    grid: {
+      top: '20%',
+    },
+    yAxis: {
+      type: 'value',
+      splitline: {
+        lineStyle: 'dashed'
+      }
+    },
+    series: [
+      {
+        data: year.map(item => item.salary),
+        type: 'line',
+        smooth: true,
+        areaStyle: {},
+        symbolSize: 5,
+        lineStyle: {
+          width: 4,
+          color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
+              // 0% 处的颜色
+              { offset: 0, color: 'red' },
+              // 50% 处的颜色
+              // { offset: 0.5, color: 'yellow' },
+              // 100% 处的颜色
+              { offset: 1, color: 'white' }],
+            global: false // 缺省为 false
+          }
+        },
+        //区域样式 
+        areaStyle: {
+          //渐变色
+          color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
+              // 0% 处的颜色
+              { offset: 0, color: 'red' },
+              // 50% 处的颜色
+              // { offset: 0.5, color: 'yellow' },
+              // 100% 处的颜色
+              { offset: 1, color: 'white' }],
+            global: false // 缺省为 false
+          }
+        }
+      }
+    ],
+    // color: [
+    //   {
+    //     type: 'linear',
+    //     x: 0,
+    //     y: 0,
+    //     x2: 0,
+    //     y2: 1,
+    //     colorStops: [
+    //       // 0% 处的颜色
+    //       { offset: 0, color: 'red' },
+    //       // 50% 处的颜色
+    //       // { offset: 0.5, color: 'yellow' },
+    //       // 100% 处的颜色
+    //       { offset: 1, color: 'white' }],
+    //     global: false // 缺省为 false
+    //   }
+    // ]
+  }
+  myChart.setOption(option)
 }
